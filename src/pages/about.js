a8b9e4db20f572950739dd41e88132637a074fb4
maryojo <maryojo3@gmail.com>
2022-03-31 15:03:46 +0100

    Editing components

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+   <Layout pageTitle="About">
+      <p>Hi there, I'm Mary!</p>
+   </Layout>
+  )
+}
+
+export default AboutPage
