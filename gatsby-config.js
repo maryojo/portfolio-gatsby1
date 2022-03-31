7bc338da7af3687b4d4f9b1beb180f72a87fa2fc
maryojo <maryojo3@gmail.com>
2022-03-31 15:46:41 +0100

    Added image

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My Personal Portfolio`,
+    description: `My personal portfolio built with Gatsby`,
+    author: `maryojo`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
    <Layout pageTitle="Homepage">
     <p>Page content here</p>
+    <StaticImage src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large" alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"></StaticImage>
    </Layout>
   )
 }
